3bf5e73927dbb9ba86bec93de4c52ad399f38d37
yesRedream <yesRedream@gmail.com>
2018-02-02 14:16:51 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('left-aside-btn').onclick = function() {
+  document.getElementById('left-aside').classList.toggle('active');
+}
+
+document.getElementById('right-aside-btn').onclick = function() {
+  document.getElementById('right-aside').classList.toggle('active');
+}
